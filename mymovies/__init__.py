025200268cd36bda1f12a02428fa710c90165888
ha <fake@example.com>
2019-07-20 22:17:54 +0900

    change heroku database uri

--- mymovies/__init__.py
+++ mymovies/__init__.py
@@ -6,7 +6,7 @@ from flask_heroku import Heroku
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'xRZgiO5v5OgSszuo7dME6A'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://lkawrazylcwjbn:06ee2fea72e90f0ee40a7687f2ff61c54e2b4c030a60798565311397d38bccad@ec2-107-20-173-2.compute-1.amazonaws.com:5432/dbgc6u09n5vnrk'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://idboceavpzxbop:2f4d8dc270a11a0fb423c103679f78d5634709f9a8111cbe7f5d29126dac63e4@ec2-54-83-1-101.compute-1.amazonaws.com:5432/dd9kj62lqu6bu7'
 heroku = Heroku(app)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
